28ae0847d886979a62d28e16786e29c1aecc32d0
= <177979888+MikeBellerue-Nagios@users.noreply.github.com>
2026-05-23 21:50:55 -0500

    More work to be done, but installs on x86_64 rhel

--- ansible/ansible_collections/nagios/ncpa/plugins/module_utils/ncpa_common.py
+++ ansible/ansible_collections/nagios/ncpa/plugins/module_utils/ncpa_common.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-from ansible.module_utils.basic import AnsibleModule
-import os
 import subprocess
 
 
@@ -64,7 +62,7 @@ changed:
 '''
 
 
-def detect_os_family():
+def detect_os_info():
     os_release = {}
 
     with open("/etc/os-release", "r") as f:
@@ -73,31 +71,63 @@ def detect_os_family():
                 k, v = line.strip().split("=", 1)
                 os_release[k] = v.strip('"')
 
-    os_id = os_release.get("ID", "").lower()
-
-    if os_id in ["rhel", "centos", "rocky", "almalinux", "fedora"]:
-        return "RedHat"
-
-    if os_id in ["ubuntu", "debian"]:
-        return "Debian"
-
-    return "Unknown"
+    return os_release.get("ID", "").lower()
 
 
-def is_ncpa_installed():
-    result = subprocess.run(
-        ["rpm", "-q", "ncpa"],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL
+def is_ncpa_installed(module):
+
+    rpm_path = module.get_bin_path(
+        "rpm"
     )
 
-    return result.returncode == 0
+    if not rpm_path:
+        module.fail_json(
+            msg="rpm binary not found"
+        )
+
+    rc, stdout, stderr = module.run_command(
+        [rpm_path, "-q", "ncpa"]
+    )
+
+    return rc == 0
+
+def build_rpm_url(version, major_release, architecture):
+    base_url = "https://assets.nagios.com/downloads/ncpa3"
+
+    if version in [None, "", "latest"]:
+        filename = f"ncpa-latest-1.{architecture}.rpm"
+    else:
+        filename = f"ncpa-{version}-1.el{major_release}.{architecture}.rpm"
+    
+    return f"{base_url}/{filename}"
+
+def parse_os_release():
+
+    os_release = {}
+
+    with open("/etc/os-release", "r") as f:
+        for line in f:
+            if "=" in line:
+                k, v = line.strip().split("=", 1)
+                os_release[k] = v.strip('"')
+
+    return os_release
+
+def detect_architecture():
+    return "x86_64"
+
+def configure_nagios_repository():
+    pass
 
 
 def install_rhel_direct(module):
     changed = False
 
-    if is_ncpa_installed():
+    version = module.params.get("version")
+    major_release = parse_os_release()
+    architecture = detect_architecture()
+
+    if is_ncpa_installed(module):
         module.exit_json(
             changed=False,
             msg="NCPA already installed"
@@ -106,10 +136,26 @@ def install_rhel_direct(module):
     if module.check_mode:
         module.exit_json(changed=True)
 
-    url = "https://assets.nagios.com/downloads/ncpa/ncpa-latest.el9.x86_64.rpm"
+    url = build_rpm_url(
+        version,
+        major_release["VERSION"],
+        architecture
+    )
+
+    if not url:
+        module.fail_json(
+            msg="failed to build url."
+        )
+
+    dnf_path = module.get_bin_path( "dnf" )
+
+    if not dnf_path:
+        module.fail_json(
+            msg="dnf binary not found"
+        )
 
     cmd = [
-        "dnf",
+        dnf_path,
         "-y",
         "install",
         url
@@ -135,34 +181,3 @@ def install_rhel_repository(module):
     module.fail_json(
         msg="Repository installation not yet implemented"
     )
-
-
-def main():
-
-    module = AnsibleModule(
-        argument_spec=dict(
-            state=dict(type='str', default='present', choices=['present', 'absent']),
-            install_method=dict(type='str', default='package', choices=['package']),
-            package_source=dict(type='str', default='direct', choices=['direct', 'repository']),
-            version=dict(type='str')
-        ),
-        supports_check_mode=True
-    )
-
-    os_family = detect_os_family()
-
-    if os_family == "RedHat":
-
-        if module.params["package_source"] == "direct":
-            install_rhel_direct(module)
-
-        elif module.params["package_source"] == "repository":
-            install_rhel_repository(module)
-
-    module.fail_json(
-        msg=f"Unsupported OS family: {os_family}"
-    )
-
-
-if __name__ == "__main__":
-    main()
--- ansible/ansible_collections/nagios/ncpa/plugins/modules/ncpa_install.py
+++ ansible/ansible_collections/nagios/ncpa/plugins/modules/ncpa_install.py
@@ -1,11 +1,24 @@
 
+from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.nagios.ncpa.plugins.module_utils.ncpa_common import (
-    detect_os_family,
-    build_rpm_url
+    install_rhel_direct
 )
 
-module.exit_json(
-    changed=False,
-    os_family=detect_os_family(),
-    rpm_url=build_rpm_url()
-)
+def main():
+    
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(type='str', default='present', choices=['present', 'absent']),
+            install_method=dict(type='str', default='package', choices=['package']),
+            package_source=dict(type='str', default='direct', choices=['direct', 'repository']),
+            version=dict(type='str')
+        ),
+        supports_check_mode=True
+    )
+
+    result = install_rhel_direct(module)
+
+    module.exit_json(**result)
+
+if __name__ == "__main__":
+    main()
